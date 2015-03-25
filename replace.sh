a974a10cd607e4c3009217c252dbd9467deed21b
Alex Bezuska <abezuska@gmail.com>
2015-03-25 10:40:56 -0400

    add replace script

---
+++ replace.sh
@@ -0,0 +1,8 @@
+# copy files into dotfiles repo
+
+cp ~/.bash_profile ./.bash_profile 
+cp ~/.bashrc ./.bashrc 
+cp ~/.gitconfig ./.gitconfig 
+cp ~/.gitignore ./.gitignore 
+cp ~/.vimrc ./.vimrc 
+cp ~/Library/Preferences/com.mizage.Divvy.plist ./divvyprefs 
